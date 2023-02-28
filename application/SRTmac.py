8ddd1777d7e454fda54b6572fdf9fcbdd4b931ad
songkang <songgao12@naver.com>
2023-02-28 11:48:18 +0900

    fix'

---
+++ application/SRTmac.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 09:19:16 2023
+
+@author: 송강
+"""
+
+
+from SRT import SRT
+import time
+srt = SRT("010-8387-8003","dksthd1@3")
+
+dep = '동대구'
+arr = '수서'
+date = '20230228'
+timee = '090000'
+
+# [[SRT] 09월 30일, 수서~부산(15:00~17:34) 특실 예약가능, 일반실 예약가능,
+# [SRT] 09월 30일, 수서~부산(15:30~18:06) 특실 예약가능, 일반실 예약가능,
+# [SRT] 09월 30일, 수서~부산(16:00~18:24) 특실 매진, 일반실 예약가능,
+# [SRT] 09월 30일, 수서~부산(16:25~18:45) 특실 예약가능, 일반실 예약가능, ...]
+reservation = None
+for j in range(10000):
+    trains = srt.search_train(dep, arr, date, timee)
+    for train in trains:
+        if train.dep_time in "142300":
+            print("ok")
+            reservation = srt.reserve(train)
+            break
+    if reservation is not None:
+        break
+    time.sleep(1)
+
+# reservation = srt.reserve(trains[1])
+# reservation
+# [SRT] 09월 30일, 수서~부산(15:30~18:06) 53700원(1석), 구입기한 09월 20일 23:38
